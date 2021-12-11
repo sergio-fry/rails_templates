87f2d90d2acd84323206ca76d12b65f011f96b7e
Sergei O. Udalov <sergei.udalov@gmail.com>
2021-12-11 10:58:52 +0300

    rspec works

--- rspec.rb
+++ rspec.rb
@@ -1,5 +1,7 @@
 run "spring stop || echo 'spring is off'"
 
+run "rm -rf test"
+
 gem_group :development, :test do
   gem "rspec-rails"
 end
@@ -7,7 +9,6 @@ end
 run "bundle install"
 rails_command "generate rspec:install"
 
+run "rm spec/spec_helper.rb"
 require "open-uri"
 file "spec/spec_helper.rb", URI.parse("https://raw.githubusercontent.com/sergio-fry/spec_helper/master/spec_helper.rb").open.read
-
-run "rm -rf test" if yes?("Do you want to remove the /test directory?")
